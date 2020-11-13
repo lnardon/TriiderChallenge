66aa6feab5f4dd551c16e439bb5283e5c4013123
lnardon <lucaslnardon@gmail.com>
2020-11-12 21:11:01 -0300

    ✨ Finish login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import "./styles.css";
 import bg from "../../assets/bg.jpeg";
 
 function LoginPage() {
+  const history = useHistory();
   return (
     <div className="loginPageContainer">
       <div className="left">
@@ -11,7 +13,32 @@ function LoginPage() {
         <img src={bg} alt="BG" className="bg" />
       </div>
       <div className="right">
-        <div className="rightContent"></div>
+        <div className="rightContent">
+          <h1 className="title">Login</h1>
+          <div className="inputDiv">
+            <label htmlFor="email">E-mail</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div className="inputDiv">
+            <label htmlFor="pass">Senha</label>
+            <input type="password" name="pass" id="pass" />
+          </div>
+          <button className="login" onClick={() => history.push("/homepage")}>
+            Entrar
+          </button>
+
+          <a href="/" className="forgotPass">
+            Esqueci minha senha
+          </a>
+
+          <div className="separator"></div>
+
+          <h4 className="subText">Não possui uma conta?</h4>
+
+          <button className="signUp" onClick={() => history.push("/signup")}>
+            Criar Conta
+          </button>
+        </div>
       </div>
     </div>
   );
