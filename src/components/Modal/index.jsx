598272eca4a82d99ad5f456473e6997626bc0677
lnardon <lucaslnardon@gmail.com>
2020-11-15 02:33:50 -0300

    🚧 Add modal component and add events fetch

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+import styles from "./styles.module.css";
+import closeIcon from "../../assets/close.svg";
+
+function Modal({ setIsOpen }) {
+  const [name, setName] = useState("");
+  const [place, setPlace] = useState("");
+  const [day, setDay] = useState("");
+  const [shift, setShift] = useState("");
+
+  async function addEvent() {
+    let respose = await fetch("http://localhost:3000/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: window.location.pathname.split("/")[2],
+        name: name,
+        week_day: day,
+        day_shift: shift,
+        address: place,
+      }),
+    });
+
+    if (respose.status === 201) {
+      setIsOpen(false);
+      alert("Evento criado");
+    } else {
+      alert("Erro ao criar evento");
+    }
+  }
+
+  return (
+    <div className={styles.modalBg}>
+      <div className={styles.modalContent}>
+        <h1 className={styles.modalTitle}>Cadastrar evento</h1>
+        <div className={styles.inputDiv}>
+          <label htmlFor="name">Nome do evento</label>
+          <img
+            src={closeIcon}
+            alt="Close"
+            onClick={() => setIsOpen(false)}
+            className={styles.closeBtn}
+          />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputDiv}>
+          <label htmlFor="place">Local</label>
+          <input
+            type="text"
+            name="place"
+            id="place"
+            onChange={(e) => setPlace(e.target.value)}
+          />
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            boxSizing: "border-box",
+          }}
+        >
+          <div
+            className={styles.inputDiv}
+            style={{ marginRight: "1rem", width: "50%" }}
+          >
+            <label htmlFor="date">Data</label>
+            <input
+              type="text"
+              name="date"
+              id="date"
+              onChange={(e) => setDay(e.target.value)}
+            />
+          </div>
+          <div className={styles.inputDiv} style={{ width: "50%" }}>
+            <label htmlFor="shift">Turno</label>
+            <input
+              type="text"
+              name="shift"
+              id="shift"
+              onChange={(e) => setShift(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <button className={styles.createBtn} onClick={addEvent}>
+          Adicionar evento
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,76 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 import styles from "./styles.module.css";
-import closeIcon from "../../assets/close.svg";
+import Modal from "../../components/Modal";
 
 function Homepage() {
-  const [days, setDays] = useState();
   const [isOpen, setIsOpen] = useState(false);
-  const [name, setName] = useState("");
-  const [place, setPlace] = useState("");
-  const [day, setDay] = useState("");
-  const [shift, setShift] = useState("");
+  const [days, setDays] = useState();
+  const [events, setEvents] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let response = await fetch("http://localhost:3000/events");
+      let parsedResponse = await response.json();
+      let events = [];
+      parsedResponse.forEach((event) => {
+        if (event.user_id === window.location.pathname.split("/")[2]) {
+          events.push(event);
+        }
+      });
+      setEvents(events);
+    })();
+  }, []);
 
   return (
     <div className={styles.homepageContainer}>
-      {isOpen ? (
-        <div className={styles.modalBg}>
-          <div className={styles.modalContent}>
-            <h1 className={styles.modalTitle}>Cadastrar evento</h1>
-            <div className={styles.inputDiv}>
-              <label htmlFor="name">Nome do evento</label>
-              <img
-                src={closeIcon}
-                alt="Close"
-                onClick={() => setIsOpen(false)}
-                className={styles.closeBtn}
-              />
-              <input
-                type="text"
-                name="name"
-                id="name"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className={styles.inputDiv}>
-              <label htmlFor="place">Local</label>
-              <input
-                type="text"
-                name="place"
-                id="place"
-                onChange={(e) => setPlace(e.target.value)}
-              />
-            </div>
-            <div
-              style={{
-                width: "100%",
-                display: "flex",
-              }}
-            >
-              <div className={styles.inputDiv}>
-                <label htmlFor="date">Data</label>
-                <input
-                  type="text"
-                  name="date"
-                  id="date"
-                  onChange={(e) => setDay(e.target.value)}
-                />
-              </div>
-              <div className={styles.inputDiv}>
-                <label htmlFor="shift">Turno</label>
-                <input
-                  type="text"
-                  name="shift"
-                  id="shift"
-                  onChange={(e) => setShift(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <button className={styles.createBtn}>Adicionar evento</button>
-          </div>
-        </div>
-      ) : null}
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
 
       <div className={styles.createDiv}>
         <button
