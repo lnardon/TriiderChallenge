86ba6eafef9dac3a99303961160dcfab36fef4db
lnardon <lucaslnardon@gmail.com>
2020-11-16 03:02:40 -0300

    ✨ Finish desktop calendar component

--- src/components/DesktopCalendar/index.jsx
+++ src/components/DesktopCalendar/index.jsx
@@ -1,18 +1,45 @@
 import React from "react";
 
 import styles from "./styles.module.css";
+import UnavailableSlot from "../UnavailableSlot";
+import AvailableSlot from "../AvailableSlot";
+import EventSlot from "../EventSlot";
 
 function DesktopCalendar({ calendarData }) {
   return (
     <div className={styles.calendarBg}>
-      asa
-      {/* {shifts.map((shift) => {
-        return (
-          <div className={styles.shiftSection}>
-            <div className={styles.available}></div>
-          </div>
-        );
-      })} */}
+      <div className={styles.calendarDiv}>
+        {calendarData.map((data, i) => {
+          return (
+            <div className={styles.dayDiv}>
+              <h1 className={styles.weekDay}>{data.title}</h1>
+              {data.available ? (
+                data.shifts.map((shift, index) => {
+                  return shift.available ? (
+                    shift.info ? (
+                      <EventSlot
+                        key={index}
+                        name={shift.info.name}
+                        address={shift.info.address}
+                      />
+                    ) : (
+                      <AvailableSlot key={index} />
+                    )
+                  ) : (
+                    <UnavailableSlot key={index} />
+                  );
+                })
+              ) : (
+                <>
+                  <UnavailableSlot />
+                  <UnavailableSlot />
+                  <UnavailableSlot />
+                </>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
