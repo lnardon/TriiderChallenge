efdd897c30c619e79b5b339080b368022ef95271
lnardon <lucaslnardon@gmail.com>
2020-11-16 00:11:47 -0300

    🎨 Add available slot component

---
+++ src/components/AvailableSlot/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+function AvailableSlot() {
+  return (
+    <div className={styles.availableSlotContainer}>
+      <h1 className={styles.title}> DISPONÍVEL</h1>
+    </div>
+  );
+}
+
+export default AvailableSlot;
